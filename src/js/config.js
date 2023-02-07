d932d9c347333c05ce9a3820ca55f100e6af3341
JaydenBBB <rhqtmf09@gmail.com>
2023-02-07 11:21:33 -0800

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 5;
+export const KEY = '4a59f250-681e-48df-b4ca-ea44c28c0660';
+export const MODAL_CLOSE_SEC = 2.5;
